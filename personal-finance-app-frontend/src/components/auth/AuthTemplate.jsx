5b30627fad6c6af6b16a02a98221c861e60d3331
Igor Dubik <whiteriver.devprojects@gmail.com>
2025-03-11 16:05:45 +1100

    Built mobile navbar component. Now need to add active and hover states

--- personal-finance-app-frontend/src/components/auth/AuthTemplate.jsx
+++ personal-finance-app-frontend/src/components/auth/AuthTemplate.jsx
@@ -4,6 +4,9 @@ import Login from './Login';
 import Register from './Register';
 import { useLocation } from 'react-router-dom';
 
+import AuthIllustration from '../../assets/images/illustration-authentication.svg';
+import LogoLarge from '../../assets/images/logo-large.svg';
+
 function AuthTemplate() {
     const location = useLocation();
 
@@ -13,11 +16,11 @@ function AuthTemplate() {
         <div className="auth-template">
             <div className="auth-template__container">
                 <div className="mobile-banner">
-                    <img src='/assets/images/logo-large.svg' alt='Logo' />
+                    <img src={LogoLarge} alt='Logo' />
                 </div>
                 <div className="auth-template__left">
-                    <img src='/assets/images/illustration-authentication.svg' alt='Authentication' />
-                    <img src='/assets/images/logo-large.svg' className='overlay-logo' alt='Logo' />
+                    <img src={AuthIllustration} alt='Authentication' />
+                    <img src={LogoLarge} className='overlay-logo' alt='Logo' />
                     <div className='overlay-text'>
                         <h1> Keep track of your money and save for your future</h1>
                         <p>Personal finance app puts you in control of your spending. Track transactions, set budgets, and add to savings pot easily.</p>
--- personal-finance-app-frontend/src/components/navigation/MobileNavbar.jsx
+++ personal-finance-app-frontend/src/components/navigation/MobileNavbar.jsx
@@ -14,24 +14,34 @@ function MobileNavbar() {
   return (
     <nav className="navbar">
       <Link to="/overview" className="navbar__item">
-        <span className="navbar__icon"><img src={OverviewIcon} alt='icon'/></span>
-        <span className="navbar__text">Overview</span>
+        <div className="navbar__item">
+          <span className="navbar__icon"><img src={OverviewIcon} alt='icon'/></span>
+          <span className="navbar__text">Overview</span>
+        </div>
       </Link>
       <Link to="/transactions" className="navbar__item">
-        <span className="navbar__icon"><img src={TransactionsIcon} alt='icon'/></span>
-        <span className="navbar__text">Transactions</span>
+        <div className="navbar__item">
+          <span className="navbar__icon"><img src={TransactionsIcon} alt='icon'/></span>
+          <span className="navbar__text">Transactions</span>
+        </div>
       </Link>
       <Link to="/budgets" className="navbar__item">
-        <span className="navbar__icon"><img src={BudgetsIcon} alt='icon'/></span>
-        <span className="navbar__text">Budgets</span>
+        <div className="navbar__item">
+          <span className="navbar__icon"><img src={BudgetsIcon} alt='icon'/></span>
+          <span className="navbar__text">Budgets</span>
+        </div>
       </Link>
       <Link to="/pots" className="navbar__item">
-        <span className="navbar__icon"><img src={PotsIcon} alt='icon'/></span>
-        <span className="navbar__text">Pots</span>
+        <div className="navbar__item">
+          <span className="navbar__icon"><img src={PotsIcon} alt='icon'/></span>
+          <span className="navbar__text">Pots</span>
+        </div>
       </Link>
       <Link to="/recurring-bills" className="navbar__item">
-        <span className="navbar__icon"><img src={RecurringBillsIcon} alt='icon'/></span>
-        <span className="navbar__text">Recurring Bills</span>
+        <div className="navbar__item">
+          <span className="navbar__icon"><img src={RecurringBillsIcon} alt='icon'/></span>
+          <span className="navbar__text">Recurring Bills</span>
+        </div>
       </Link>
     </nav>
   );
