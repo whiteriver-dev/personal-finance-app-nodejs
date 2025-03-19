c190b535362551674c5dd04fdf7e63fa902007ee
Igor Dubik <whiteriver.devprojects@gmail.com>
2025-03-19 21:09:32 +1100

    Created and finished Search component that reorders data according to letters in the input

--- personal-finance-app-frontend/src/components/dashboard/Transactions.jsx
+++ personal-finance-app-frontend/src/components/dashboard/Transactions.jsx
@@ -1,10 +1,12 @@
 import './Transactions.scss';
 import React from 'react';
+import Search from './small/Search';
 
 function Transactions() {
     return (
         <div className="transactions">
             <h1>Transactions</h1>
+            <Search placeholder="Search transaction"/>
         </div>
     );
 }
