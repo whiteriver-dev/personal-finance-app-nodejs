c2a771686866834913d77d0295b53c0eab19bc16
Igor Dubik <whiteriver.devprojects@gmail.com>
2025-04-07 20:39:01 +1000

    Added legend to the Donut Chart component - needs styling and polishing

--- personal-finance-app-frontend/src/components/dashboard/small/DonutChart.jsx
+++ personal-finance-app-frontend/src/components/dashboard/small/DonutChart.jsx
@@ -4,13 +4,14 @@ import './DonutChart.scss';
 
 function DonutChart({ data, width, height }) {
   const ref = useRef();
+  const colors = ['#277C78', '#82C9D7', '#F2CDAC', '#626070', '#C94736', '#AF81BA', '#597C7C']
 
 
   useEffect(() => {
     if (!data || data.length === 0) return;
 
     const radius = Math.min(width, height) / 2;
-    const colors = ['#277C78', '#82C9D7', '#F2CDAC', '#626070', '#C94736', '#AF81BA', '#597C7C']
+
 
     const svg = d3.select(ref.current);
     svg.selectAll("*").remove(); // Clear previous renders
@@ -27,10 +28,8 @@ function DonutChart({ data, width, height }) {
 
     // Arc generator
     const arc = d3.arc()
-      .innerRadius(radius * 0.55) // Donut thickness
+      .innerRadius(radius * 0.55) // 
       .outerRadius(radius * 0.9);
-
-    // Color scale (fallback if no props)
 
     // Draw slices
     chart
@@ -39,25 +38,42 @@ function DonutChart({ data, width, height }) {
       .join('path')
       .attr('d', arc)
       .attr('fill', (d, i) => colors[i % colors.length])
-      .attr('stroke', '#fff')
-      .style('stroke-width', '2px');
 
     const innerOverlayArc = d3.arc()
       .innerRadius(radius * 0.5)
-      .outerRadius(radius * 0.66); // inner third (0.5 + one third of 0.4 thickness)
+      .outerRadius(radius * 0.66); 
     
     chart
       .selectAll('inner-overlay')
       .data(data_ready)
       .join('path')
       .attr('d', innerOverlayArc)
-      .attr('fill', '#fff') // or a highlight color like "#000"
+      .attr('fill', '#fff') //
       .attr('opacity', 0.25);
       
 
-  }, [data, width, height]);
+  }, [data, width, height, colors]);
 
-  return <svg ref={ref}></svg>;
+
+  return (
+    <div className='donut-chart'>
+        <svg ref={ref}></svg>;
+        <div className="donut-legend">
+            {data.map((budget, i) => (
+            <div key={i} className="donut-legend__item">
+                <span
+                    className="donut-legend__label"
+                    style={{ borderLeft: `0.6rem solid ${colors[i % colors.length]}` }}
+                >
+                    {budget.name}
+                </span>
+            </div>
+            ))}
+        </div>
+  </div>
+
+
+  ) 
 }
 
 export default DonutChart;
