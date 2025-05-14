23e67562755707cb289615e909274143c206f48b
Igor Dubik <whiteriver.devprojects@gmail.com>
2025-05-14 22:40:35 +1000

    Begin working on Overview component of dashboard

--- personal-finance-app-frontend/src/components/dashboard/Overview.jsx
+++ personal-finance-app-frontend/src/components/dashboard/Overview.jsx
@@ -4,7 +4,24 @@ import React from 'react';
 function Overview() {
     return (
         <div className="overview">
-            <h1>Overview</h1>
+            <h1 className='overview__header'>Overview</h1>
+            <div className='balance-income-expenses'>
+                <div className='balance-income-expenses__item dark'>
+                    <h2 className='balance-income-expenses__item-header'>Current Balance</h2>
+                    <p className='balance-income-expenses__item-value'>$0.00</p>
+                </div>
+                <div className='balance-income-expenses__item'>
+                    <h2 className='balance-income-expenses__item-header'>Income</h2>
+                    <p className='balance-income-expenses__item-value'>$0.00</p>
+                </div>
+                <div className='balance-income-expenses__item'>
+                    <h2 className='balance-income-expenses__item-header'>Expenses</h2>
+                    <p className='balance-income-expenses__item-value'>$0.00</p>
+                </div>
+            </div>
+            <div className='overview__grid'>
+
+            </div>
         </div>
     );
 }
