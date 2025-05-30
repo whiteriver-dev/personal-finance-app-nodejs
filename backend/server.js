a348ca4bb05915193dd6415653be265de4f56bcf
Igor Dubik <whiteriver.devprojects@gmail.com>
2025-05-30 19:43:18 +1000

    Added Test endpoint

--- backend/server.js
+++ backend/server.js
@@ -530,6 +530,10 @@ app.delete('/pots/:id', async (req, res) => {
   }
 });
 
+app.get('/test', (req, res) => {
+  res.status(200).json({ message: 'API is working!' });
+});
+
 // ---- SERVER LISTEN ----
 const PORT = process.env.PORT || 5050;
 app.listen(PORT, '0.0.0.0', () => {
