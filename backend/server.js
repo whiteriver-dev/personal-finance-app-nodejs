4ca52f419cf3006e201836e9bde0dc2438e7a892
Igor Dubik <whiteriver.devprojects@gmail.com>
2025-06-03 15:02:30 +1000

    Modified CORS settings

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['https://project1.whiteriver-dev.com', 'personal-finance-app-nodejs-e1geysihk-whiteriver-devs-projects.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
